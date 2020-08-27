5849a6282b58d93359de44c08a58b9a3b8e61d92
justinnoble21 <21noble.justin@scsmustangs.org>
2020-08-27 18:08:57 +0000

    Tried to do some stuff to get familiar with C#. Got close enough, one error :)

--- main.cs
+++ main.cs
@@ -2,6 +2,23 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("What is your name?");
   }
+
+  public void respond(string input) {
+    if (input == " ") 
+    {
+      write("That's blank!");
+      return;
+    }
+
+    string name = char.ToUpper(input[0]) + name.Substring(1);
+
+    write($"Oh, I see! {name}... Nice name!");
+  }
+
+  public void write(string input) {
+    Console.WriteLine(input);
+  }
+
 }
