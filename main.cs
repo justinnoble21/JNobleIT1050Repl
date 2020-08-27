3c1d9b6d6b5d8bbb85ebdcce053b86ddaec0727f
justinnoble21 <21noble.justin@scsmustangs.org>
2020-08-27 18:23:43 +0000

    Works now :)

--- main.cs
+++ main.cs
@@ -3,21 +3,27 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("What is your name?");
+    string input = Console.ReadLine();
+    respond(input);
+  
   }
 
-  public void respond(string input) {
+  public static void respond(string input) {
     if (input == " ") 
     {
       write("That's blank!");
       return;
     }
 
-    string name = char.ToUpper(input[0]) + name.Substring(1);
+
+    char c = char.ToUpper(input[0]);
+
+    string name = c + input.Substring(1);
 
     write($"Oh, I see! {name}... Nice name!");
   }
 
-  public void write(string input) {
+  public static void write(string input) {
     Console.WriteLine(input);
   }
 
